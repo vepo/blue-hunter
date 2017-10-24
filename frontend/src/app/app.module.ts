581d2560a95aac674674bab1d9f20a3bac58ac42
Victor Osorio <victor.perticarrari@gmail.com>
2017-10-24 17:15:42 -0200

    Title

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchByNameComponent } from './search-by-name/search-by-name.component';
 import { SearchByTitleComponent } from './search-by-title/search-by-title.component';
-
+import { BookService } from './book.service';
 const appRoutes: Routes = [
   { path: '', component: SearchByNameComponent },
   { path: 'name', component: SearchByNameComponent },
@@ -29,11 +29,12 @@ export const routing = RouterModule.forRoot(appRoutes);
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/search-by-title/search-by-title.component.ts
+++ frontend/src/app/search-by-title/search-by-title.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-search-by-title',
   templateUrl: './search-by-title.component.html',
-  styleUrls: ['./search-by-title.component.css']
+  styleUrls: ['./search-by-title.component.less']
 })
-export class SearchByTitleComponent implements OnInit {
+export class SearchByTitleComponent {
+  private term: string;
+  private lastSearchTerm: string;
+  private books: any = null;
+  private loading: boolean = false;
+  constructor(private bookService: BookService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  search() {
+    this.lastSearchTerm = this.term;
+    this.loading = true;
+    this.bookService.search(this.term).subscribe(books => {
+      this.books = books;
+      this.loading = false;
+    });
   }
 
 }
